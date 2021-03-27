c51f219fc91042f15613b413ea782d4defafc0f6
Abarajithan Ramachandran <abarajithan.r@olacabs.com>
2021-03-28 01:01:54 +0530

    Data Handling

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-
 import { Bar } from 'react-chartjs-2';
+
 const Chart = (props) => {
     
+    let [showToast, setShowToast]  = useState(false)
     let [yAxisData, updateYAxisData]  = useState([])
     let [xAxisData, updateXAxisData]  = useState([])
     let [randomColors, setRandomColors] = useState([]);
@@ -16,44 +17,60 @@ const Chart = (props) => {
 
     useEffect(()=>{
         if(props.data.items && props.data.items.length){
+            
             let xAxisData = [];
             let yAxisData = [];
-            props.data.items.map(item=> {
+            props.data.items.forEach(item => {
                 yAxisData.push(item.count)
                 xAxisData.push(item.name)
                 randomColors.push(generateColorCodes())
             })
+            setShowToast(false);
             updateYAxisData(yAxisData);
             updateXAxisData(xAxisData);
             setRandomColors(randomColors);
         }
+        else{
+            setShowToast(true);
+        }
     },[props])
     return(
-        <div className="chart-container">
-            <Bar
-                data={{
-                        labels: xAxisData,
-                        datasets: [{
-                            label: 'Language',
-                            data: yAxisData,
-                            backgroundColor: randomColors,
-                        }]
+        <React.Fragment>
+        {
+            showToast && 
+            <div class="alert alert-secondary" role="alert">
+                No Details found for this query
+            </div>
+        }
+        {
+            !showToast && 
+            <div className="chart-container">
+                <Bar
+                    data={{
+                            labels: xAxisData,
+                            datasets: [{
+                                label: 'Language',
+                                data: yAxisData,
+                                backgroundColor: randomColors,
+                            }]
+                        }
                     }
-                }
-                width={50}
-                height={50}
-                options={{ 
-                    maintainAspectRatio: false,
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }}
-            />
-        </div>
+                    width={50}
+                    height={50}
+                    options={{ 
+                        maintainAspectRatio: false,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }}
+                />
+            </div>
+        }
+        </React.Fragment>
     )  
 }
 
